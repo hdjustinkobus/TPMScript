70dee7b954dd31f1084ce2b140012d1b1a7b9a07
hdjustinkobus <justin.kobus@harley-davidson.com>
2023-09-18 12:49:33 -0500

    Create DisableAndReEnableTPM.ps1

---
+++ DisableAndReEnableTPM.ps1
@@ -0,0 +1,16 @@
+﻿#To disable TPM
+Suspend-BitLocker -MountPoint "C:" -RebootCount 1 -ErrorAction Continue
+(Get-WmiObject -Class Lenovo_SetBiosSetting -Namespace root\wmi).SetBiosSetting("SecurityChip,Disable")
+(Get-WmiObject -Class Lenovo_SaveBiosSettings -Namespace root\wmi).SaveBiosSettings()
+
+
+#Then Reboot
+
+#To turn TPM back on
+(Get-WmiObject -Class Lenovo_SetBiosSetting -Namespace root\wmi).SetBiosSetting("SecurityChip,Enable$Password1")
+(Get-WmiObject -Class Lenovo_SaveBiosSettings -Namespace root\wmi).SaveBiosSettings($Password2)
+
+#Reboot again and verify that when running tpm.msc as an admin TPM shows visable.
+
+#Get BIOS Information in case it's needed
+gwmi -class Lenovo_BiosSetting -namespace root\wmi | ForEach-Object {if ($_.CurrentSetting -ne "") {Write-Host $_.CurrentSetting.replace(","," = ")}}
